584544b9ddc38ed497eb0f7894a5f07f404814a2
suresh <suresh321996@gmail.com>
2022-11-10 22:37:54 +0530

    javascript file is added

---
+++ javascript/greater_number.js
@@ -0,0 +1,36 @@
+let a = 20;
+    b =90;
+    c=100;
+
+  if (a>b){
+    console.log("A  is greater than B");
+
+  }
+
+  else if (c>b){
+    console.log(a+b);
+            if (b>c) 
+            {
+                console.log("b is grater than C");
+            }
+            else{
+                console.log("C is greater than B");
+            }
+  }
+  else{
+    
+        console.log("c is greater")
+    }
+  
+switch (a,b,c){
+    case a>b: console.log("c is also big");
+    break;
+    case b>c: consle.log("B is geater than C")
+    break;
+    default: console.log("c is greater than A and B")
+}
+
+
+ 
+
+  
